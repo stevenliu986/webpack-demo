778ed54fa8152534386d1a0a52c89845ff329a12
StevenLiu <stevenliu986@gmail.com>
2024-05-24 22:41:42 +1000

    update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports={
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader'],
+            }
         ]
     },
     // 插件
