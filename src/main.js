46a7d5867adcbb7c28dd678f913b34c08271a479
StevenLiu <stevenliu986@gmail.com>
2024-05-24 23:00:13 +1000

    import style

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import count from "./js/count";
 import sum from "./js/sum";
 import "./css/index.css"
 import "./less/index.less"
+import "./sass/index.sass"
+import "./sass/index.scss"
+import "./stylus/index.styl"
 
 console.log(count(3, 2));
 console.log(sum(1, 2, 3, 4));
