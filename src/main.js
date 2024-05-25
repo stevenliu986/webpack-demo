8c9225fee39fe997cc396aa9b87445c7c438a431
Steven Liu <stevenliu986@gmail.com>
2024-05-25 11:28:21 +1000

    output files

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import count from "./js/count";
 import sum from "./js/sum";
-import "./css/index.css"
-import "./less/index.less"
-import "./sass/index.sass"
-import "./sass/index.scss"
-import "./stylus/index.styl"
+import "./css/index.css";
+import "./less/index.less";
+import "./sass/index.sass";
+import "./sass/index.scss";
+// import "./stylus/index.styl"
 
 console.log(count(3, 2));
 console.log(sum(1, 2, 3, 4));
